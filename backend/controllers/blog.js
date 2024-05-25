217d0730c1e9e81646b271752fb086ce1e0253cd
JoshuaMaduri <jmaduri@hotmail.com>
2024-05-25 15:58:59 -0400

    Fixed Categories and Tags issue with Blog posting

--- backend/controllers/blog.js
+++ backend/controllers/blog.js
@@ -3,6 +3,7 @@ const Category = require('../models/category');
 const Tag = require('../models/tags');
 const formidable = require('formidable');
 const slugify = require('slugify');
+const stripHtml = require('string-strip-html');
 const _ = require('lodash');
 const { errorHandler } = require('../helpers/dbErrorHandler');
 const fs = require('fs');
@@ -10,7 +11,7 @@ const fs = require('fs');
 exports.create = async (req, res) => {
     let form = new formidable.IncomingForm();
     form.keepExtensions = true;
-    form.multiples = true; // Allow multiple files
+
     form.parse(req, async (err, fields, files) => {
         if (err) {
             return res.status(400).json({
@@ -18,69 +19,75 @@ exports.create = async (req, res) => {
             });
         }
 
-        console.log('Fields:', fields);
-        console.log('Files:', files);
-        console.log(req.auth._id)
-
         const { title, body, categories, tags } = fields;
 
-        if (!title || !Array.isArray(title) || typeof title[0] !== 'string' || title[0].length == 0) {
+        if (!title || title.trim().length === 0) {
             return res.status(400).json({
-                error: 'Title is required and should be a string'
+                error: 'Title is required'
             });
         }
 
-        if (!body || !Array.isArray(body) || typeof body[0] !== 'string' || body[0].length == 0) {
+        if (!body || body.trim().length < 200) {
             return res.status(400).json({
-                error: 'Body is required and should be a string'
+                error: 'Content is too short'
             });
         }
 
-        if (!categories || !Array.isArray(categories) || typeof categories[0] !== 'string' || categories[0].length == 0) {
+        if (!categories || categories.split(',').length === 0) {
             return res.status(400).json({
                 error: 'At least one category is required'
-            })
-        };
+            });
+        }
 
-        if (!tags || !Array.isArray(tags) || typeof tags[0] !== 'string' || tags[0].length == 0) {
+        if (!tags || tags.split(',').length === 0) {
             return res.status(400).json({
                 error: 'At least one tag is required'
-            })
-        };
-
-        // Dynamically import the string-strip-html module
-        const { stripHtml } = await import('string-strip-html');
-
-        
+            });
+        }
 
         let blog = new Blog();
-        blog.title = title[0];
-        blog.body = body[0];
-        blog.slug = slugify(title[0]).toLowerCase();
-        blog.mtitle = `${title[0]} | ${process.env.APP_NAME}`;
-        blog.mdesc = stripHtml(body[0].substring(0, 160)).result;
+        blog.title = title;
+        blog.body = body;
+        blog.slug = slugify(title).toLowerCase();
+        blog.mtitle = `${title} | ${process.env.APP_NAME}`;
+        blog.mdesc = stripHtml(body.substring(0, 160)).result;
         blog.postedBy = req.auth._id;
-        //blog.categories = categories;
-        //blog.tags = tags;
 
-        let arrayOfCategories = categories && categories.split(',');
-        let arrayOfTags = tags && tags.split(',');
+        let arrayOfCategories = categories.split(',');
+        let arrayOfTags = tags.split(',');
 
+        console.log(arrayOfCategories)
+        console.log(arrayOfTags)
 
         if (files.photo) {
-            if (files.photo[0].size > 10000000) {
+            if (files.photo.size > 10000000) {
                 return res.status(400).json({
-                    error: 'Image should be less than 10mb in size'
+                    error: 'Image should be less than 1mb in size'
                 });
             }
-            blog.photo.data = fs.readFileSync(files.photo[0].filepath);
-            blog.photo.contentType = files.photo[0].mimetype;
+            blog.photo.data = fs.readFileSync(files.photo.path);
+            blog.photo.contentType = files.photo.type;
         }
 
         try {
+
+
             const result = await blog.save();
-            res.json(result);
+
+            const categoryIds = await Category.find({ name: { $in: arrayOfCategories } }).distinct('_id');
+            const tagIds = await Tag.find({ name: { $in: arrayOfTags } }).distinct('_id');
+
+            // Update blog with category IDs
+            const updatedBlogWithCategoriesAndTags = await Blog.findByIdAndUpdate(
+                result._id,
+                { $push: { categories: { $each: categoryIds }, tags: {$each: tagIds} } }, // Push category IDs into the categories array
+                { new: true }
+            );
+        
+           res.json(updatedBlogWithCategoriesAndTags)
+
         } catch (err) {
+            console.log(err);
             return res.status(400).json({
                 error: errorHandler(err)
             });

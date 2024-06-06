081f8a99710efd0ae66de98ca95892a7dc80659d
JoshuaMaduri <jmaduri@hotmail.com>
2024-06-06 16:37:51 -0400

    Added States for checked categories and tags

--- frontend/components/crud/BlogCreate.js
+++ frontend/components/crud/BlogCreate.js
@@ -23,6 +23,13 @@ const CreateBlog = ({router}) => {
             return false;
         }
     }
+
+    const [categories, setCategories] = useState([])
+    const [tags, setTags] = useState([])
+
+    const [checked, setChecked] = useState([])
+    const [checkedTags, setCheckedTags] = useState([])
+
     const [body, setBody] = useState(blogFromLS())
     const [values, setValues] = useState({
         error: '',
@@ -37,7 +44,32 @@ const CreateBlog = ({router}) => {
 
     useEffect(() => {
         setValues({...values, formData: new FormData()});
+        initCategories();
+        initTags();
     }, [router] );
+
+
+    const initCategories = () => {
+        getCategories().then(data => {
+            if(data.error) {
+                setValues({...values, error: data.error})
+            } else {
+                setCategories(data);
+            }
+        })
+    }
+
+    const initTags = () => {
+        getTags().then(data => {
+            if(data.error) {
+                setValues({...values, error: data.error})
+            }else{
+                setTags(data);
+            }
+        })
+        
+    }
+
 
     const publishBlog = (e) => {
         e.preventDefault();
@@ -60,6 +92,45 @@ const CreateBlog = ({router}) => {
         }
     }
 
+    const handleToggle = c => () => {
+        setValues({ ...values, error: '' });
+        // return the first index or -1
+        const clickedCategory = checked.indexOf(c);
+        const all = [...checked];
+        console.log(clickedCategory)
+        console.log(all)
+
+        if (clickedCategory === -1) {
+            all.push(c);
+        } else {
+            all.splice(clickedCategory, 1);
+        }
+        console.log(all);
+        setChecked(all);
+        formData.set('categories', all);
+    };
+
+    const showCategories = () => {
+        return (
+            categories && categories.map((c, i) => (
+                <li key={i} className='list-unstyled'>
+                    <input onChange={handleToggle()} type='checkbox' className='mr-2'/>
+                    <label className='form-check-label'>{c.name}</label>
+                </li>
+            ))
+        )
+    }
+
+    const showTags = () => {
+        return (
+            tags && tags.map((t, i) => (
+                <li key={i} className='list-unstyled'>
+                    <input type='checkbox' className='mr-2'/>
+                    <label className='form-check-label'>{t.name}</label>
+                </li>
+            ))
+        )
+    }
 
 
     const createBlogForm = () => {
@@ -83,9 +154,39 @@ const CreateBlog = ({router}) => {
     }
 
     return (
-        <div>
-            {createBlogForm()}
-            <hr/>
+        <div className='container-fluid'>
+            <div className='row'>
+                <div className='col-md-8'>
+                    {createBlogForm()}
+                    <hr/>
+                {JSON.stringify(title)}
+                <hr/>
+                {JSON.stringify(body)}
+                <hr/>
+                {JSON.stringify(categories)}
+                <hr/>
+                {JSON.stringify(tags)}
+                </div>
+                
+
+                <div className='col-md-4'>
+                    <div>
+                        <h5>Categories</h5>
+                        <hr/>
+                        <ul style={{maxHeight: '200px', overflowY: 'scroll'}}>
+                            {showCategories()}
+                        </ul>
+                    </div>
+                    <br></br>
+                    <div>
+                        <h5>Tags</h5>
+                        <hr/>
+                        <ul style={{maxHeight: '200px', overflowY: 'scroll'}}>
+                            {showTags()}
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };

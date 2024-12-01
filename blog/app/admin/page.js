fcb93b5c1497756b7fc5b6a2b5e1920e7913867f
Joshua Maduri <jmaduri@hotmail.com>
2024-11-30 19:00:49 -0500

    minor update

--- blog/app/admin/page.js
+++ blog/app/admin/page.js
@@ -1,6 +1,7 @@
-import prisma from '../lib/prisma'
+ import { Search } from "../lib/components/CategorySearch"
  
  const Admin = () => {
+
 
     return (
         <div className="mt-10 p-10 ">
@@ -11,7 +12,6 @@ import prisma from '../lib/prisma'
                     <input
                         type="text"
                         name="title"
-                        // value={formData.title}
                         className="w-full p-2 mt-2"
                         required
                     />
@@ -21,86 +21,19 @@ import prisma from '../lib/prisma'
                 <div className="flex flex-row justify-between items-center">
 
                     <div className="input-group">
-                        <select className="select select-bordered">
-                            <option disabled selected>Pick category</option>
+                        <select className="select select-bordered" defaultValue="" required>
+                            <option disabled value="">Pick category</option>
                             <option>T-shirts</option>
                             <option>Mugs</option>
                         </select>
 
-                        <div className="dropdown">
-                            <div tabIndex={0} role="button" className="btn m-1 text-lg">+</div>
-                            <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
-                                <li><a>Add a Category</a></li>
-                                <li><a>Edit a Category</a></li>
-                            </ul>
-                        </div>
+                        <button className='btn btn-secondary ml-3 text-lg'>+</button>
                     </div>
 
                     <div className="input-group">
-                        <label>
-                            Tags:
-                            <div className="flex flex-wrap p-2 border border-solid border-current rounded-full mt-2">
-                            {/* {tags.map((tag, index) => (
-                                <div
-                                key={index}
-                                style={{
-                                    backgroundColor: '#0070f3',
-                                    color: '#fff',
-                                    padding: '0.25rem 0.5rem',
-                                    borderRadius: '3px',
-                                    marginRight: '0.5rem',
-                                    marginBottom: '0.5rem',
-                                    display: 'flex',
-                                    alignItems: 'center',
-                                }}
-                                >
-                                <span>{tag}</span>
-                                <button
-                                    // onClick={() => handleRemoveTag(index)}
-                                    style={{
-                                    backgroundColor: 'transparent',
-                                    color: '#fff',
-                                    border: 'none',
-                                    marginLeft: '0.5rem',
-                                    cursor: 'pointer',
-                                    }}
-                                >
-                                    ×
-                                </button>
-                                </div>
-                            ))} */}
-                            </div>
-                            <textarea
-                            // value={inputValue}
-                            // onChange={(e) => setInputValue(e.target.value)}
-                            // onKeyDown={handleKeyDown}
-                            // placeholder={tags.length >= 5 ? 'Tag limit reached!' : 'Press Enter or Comma to add tags'}
-                            // disabled={tags.length >= 5}
-                            style={{
-                                width: '100%',
-                                padding: '0.5rem',
-                                marginTop: '0.5rem',
-                                resize: 'none',
-                                height: '50px',
-                                // borderColor: tags.length >= 5 ? '#d32f2f' : '#ddd',
-                            }}
-                            />
-                        </label>
-                        {/* {tags.length >= 5 && (
-                            <p style={{ color: '#d32f2f', marginTop: '0.5rem' }}>
-                                You can add a maximum of 5 tags.
-                            </p>
-                        )} */}
-                        <div className="dropdown">
-                            <div tabIndex={0} role="button" className="btn m-1 text-lg">+</div>
-                            <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
-                                <li><a>Add a Category</a></li>
-                                <li><a>Edit a Category</a></li>
-                            </ul>
-                        </div>
-
-
+                        <Search placeholder="Emter tag name"/>
                     </div>
+                    
                 </div>
 
                 <div className="mb-1">
